b4ee1cf7fa696fe0798c9598ec5fae337095a8fd
ryutah <ryuta0221@gmail.com>
2019-03-24 17:25:14 +0900

    refactor: リソース作成スクリプトなどの配置先を変更

--- resource/provisioning.sh
+++ resource/provisioning.sh
@@ -6,3 +6,7 @@ gcloud deployment-manager deployments create \
   --config deployment.yaml \
   --automatic-rollback-on-error \
   `gcloud config get-value project`-tgtc-fileupload-example
+
+if ! gcloud beta tasks queues describe tgtc-sample-queue >/dev/null 2>&1; then
+  gcloud --quet beta tasks queues create-app-engine-queue tgtc-sample-queue
+fi
